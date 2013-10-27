b3ef8924d39b6d2b78443622c732cc0dc4744749
amthompson <andrew.thompson@mines.sdsmt.edu>
2013-10-27 17:26:06 -0600

    Revert "Alerts for saving empty contact and deleting contact added.  String resources added for the messages.  Also commented ViewlListFragement and added javadocs."
    This reverts commit 498bc42f53e3b7a5ae893586e1e96eb5d809ad74.

--- src/sdsmt/edu/thompsonsamson/assignment2/MainActivity.java
+++ src/sdsmt/edu/thompsonsamson/assignment2/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 
 public class MainActivity extends Activity implements IContactControlListener {
@@ -57,19 +58,24 @@ public class MainActivity extends Activity implements IContactControlListener {
 		_model = Model.getInstance(this);
 		
 		// refresh the list of contacts
-		refreshArrayAdapter();		
+		refreshArrayAdapter();
+		
 	}
 
 	@Override
-	public void selectContact(Contact contact) {		
-	
+	public void selectContact(Contact contact) {
+
+		Log.d("Assignment2","Select Contact: " + contact.Name + "(" + contact.ID + ")");
+		
 		_contact = contact;
 		showDetailFragment();
-	
+		
 	}
 
 	@Override
 	public void updateContact(Contact contact) {
+
+		Log.d("Assignment2","Update Contact: " + contact.Name);
 		
 		_adapter.remove(contact);
 		_adapter.add(contact);
@@ -78,11 +84,13 @@ public class MainActivity extends Activity implements IContactControlListener {
 		
 		_model.updateContact(contact);
 		_fragmentManager.popBackStackImmediate();
-	
+		
 	}
 
 	@Override
 	public void insertContact() {
+
+		Log.d("Assignment2","New Contact");
 		
 		// instantiate a new empty course object
 		_contact = new Contact();
@@ -93,6 +101,8 @@ public class MainActivity extends Activity implements IContactControlListener {
 
 	@Override
 	public void insertContact(Contact contact) {
+
+		Log.d("Assignment2","Insert Contact: " + contact.Name);
 		
 		_adapter.add(contact);
 		_adapter.sort(contact);
@@ -105,6 +115,8 @@ public class MainActivity extends Activity implements IContactControlListener {
 
 	@Override
 	public void deleteContact(Contact contact) {
+
+		Log.d("Assignment2","Delete Contact: " + contact.Name);
 		
 		_adapter.remove(contact);
 		_adapter.sort(contact);
@@ -117,9 +129,12 @@ public class MainActivity extends Activity implements IContactControlListener {
 
 	@Override
 	public Contact getContact() {
+
+		Log.d("Assignment2","Get Contact: " + _contact.Name);
+		
 		return _contact;
 	}
-	
+
 	@Override
 	public ArrayAdapter<Contact> getContactArrayAdapter() {		
 		return _adapter;
